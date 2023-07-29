bc3318ba6f2fc5460b370ed56d23615fe9eeca3a
Mr-TM-YT <www.mohamedsalwa13@gmail.com>
2023-07-30 01:14:44 +0300

    Made an example bouncing ball

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Core.Game;
 
-
 Game game = new Game(800, 600, "Hello, window");
 while (game.IsRunning())
 {
+  game.Update(Raylib_cs.Raylib.GetFrameTime());
   game.Render();
 }
 game.Clean();
--- src/Game.cs
+++ src/Game.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Raylib_cs;
 
 namespace Core
@@ -6,20 +7,45 @@ namespace Core
   {
     class Game
     {
+      private Vector2 _ballPos;
+      private Vector2 _ballVel;
+      private const int _ballRadius = 20;
+      private const int _ballSpeed = 50;
       public Game(int width, int height, string title)
       {
         Raylib.InitWindow(width, height, title);
+        Raylib.SetTargetFPS(60);
+        _ballPos.X = Raylib.GetScreenWidth() / 2;
+        _ballPos.Y = Raylib.GetScreenHeight() / 2;
+        _ballVel.X = 1;
+        _ballVel.Y = 1;
+
       }
       public bool IsRunning()
       {
         return !Raylib.WindowShouldClose();
       }
 
+      public void Update(float dt)
+      {
+        _ballPos.X += _ballVel.X * _ballSpeed * dt;
+        _ballPos.Y += _ballVel.Y * _ballSpeed * dt;
+        if (_ballPos.X <= 0 + _ballRadius || _ballPos.X >= Raylib.GetScreenWidth() - _ballRadius)
+        {
+          _ballVel.X *= -1;
+        }
+        if (_ballPos.Y <= 0 + _ballRadius || _ballPos.Y >= Raylib.GetScreenHeight() - _ballRadius)
+        {
+          _ballVel.Y *= -1;
+        }
+      }
+
       public void Render()
       {
         Raylib.BeginDrawing();
-        Raylib.ClearBackground(Color.RAYWHITE);
-        Raylib.DrawText("Hello, world", 12, 12, 20, Color.BLACK);
+        Raylib.ClearBackground(Color.BLACK);
+        Raylib.DrawText("Hello, world", 12, 12, 20, Color.RAYWHITE);
+        Raylib.DrawCircleV(_ballPos, _ballRadius, Color.BLUE);
         Raylib.EndDrawing();
       }
 
